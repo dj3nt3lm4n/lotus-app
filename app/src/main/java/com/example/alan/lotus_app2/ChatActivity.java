28c71944f880d1aca740ef745f7789df51c45a17
alan <iruga.dmc@gmail.com>
2018-03-14 22:26:15 -0700

    Merge branch 'develop' of /home/alan/AndroidStudioProjects/lotus-app with conflicts.

---
+++ app/src/main/java/com/example/alan/lotus_app2/ChatActivity.java
@@ -0,0 +1,39 @@
+package com.example.alan.lotus_app2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class ChatActivity extends AppCompatActivity {
+
+    String activeUser="";
+
+    public void sendChat(View view){
+
+        EditText chatEditText = (EditText) findViewById(R.id.chatEditText);
+
+        ParceObject message = ParceObject("message");
+
+        message.put("sender", ParceUse.g)
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        Intent intent = getIntent();
+
+        activeUser = intent.getStringExtra("username");
+
+        setTitle("chat with"+ activeUser);
+
+        Log.i("info",activeUser);
+    }
+}
